f725c72a49ff7697a531bab33cfe899c1c397d0b
Riccardo <riccardo_london@live.it>
2019-03-22 09:22:49 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,16 @@
 require 'bundler'
+require 'pry'
 Bundler.require
 
 require_relative '../lib/student'
 
+
+Nico = Student.new(name: "Ric", grade: "8th")
+
+
+
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+
+
+binding.pry
+0
--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,15 @@
 class Student
+attr_accessor :name, :grade, :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+def initialize(name:, grade:, id: nil)
+  @name = name
+  @grade = grade
+  @id = id
+end
+
+    def self.all
+    students_hashes = DB.execute "SELECT * FROM students;"
+    end
+
+
 end
